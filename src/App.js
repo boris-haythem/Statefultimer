586fe0241e881941915795d83053d59acdb712cf
boris-haythem <haythem.gharrad@gmail.com>
2019-12-30 09:25:55 +0100

    seconde commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Chrono from './components/chrono'
+
+function App() {
+  return (
+    <div className="App">
+     <Chrono/>
+    </div>
+  );
+}
+
+export default App;
